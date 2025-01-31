b2ec9df16c5826acceacf39ddd00e073fa343509
Rishab Sharma <167351217+rishabsharma07@users.noreply.github.com>
2025-02-01 00:18:42 +0530

    connect to mongo atlas

---
+++ src/index.js
@@ -0,0 +1,32 @@
+// require('dotenv').config({path: "./env"})
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+
+
+dotenv.config({
+    path: './env'
+})
+
+
+connectDB()
+
+
+
+
+/*
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants";
+
+( async () => {
+    try{
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+    }
+    catch(error){
+        console.log("error: ",error);
+        throw error
+        
+    }
+
+})()    
+
+*/
