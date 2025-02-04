22c31fdd43d539f40f94cce965d04da099c80669
Rishab Sharma <167351217+rishabsharma07@users.noreply.github.com>
2025-02-04 23:11:11 +0530

    utils code

--- src/index.js
+++ src/index.js
@@ -9,6 +9,21 @@ dotenv.config({
 
 
 connectDB()
+.then(()=>{
+    app.on(error,()=>{
+        console.log(`ERROR: ${error}`);
+        throw error
+        
+    })
+    app.listen(process.env.PORT ||  8000,()=>{
+        console.log(`Server running at port ${process.env.PORT}`);
+    })
+})
+
+.catch((error)=>{
+    console.log("Mongo db connection failed: ", error);
+    
+})
 
 
 
