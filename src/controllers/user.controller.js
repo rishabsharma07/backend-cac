d4a62dd31a594a15dbb5be227f4c060d45dd9ab3
Rishab Sharma <167351217+rishabsharma07@users.noreply.github.com>
2025-02-11 00:09:44 +0530

    register controller fix

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -54,7 +54,7 @@ const registerUser=asyncHandler(async(req,res)=>{
     }
     
     //3
-    const existedUser= User.findOne({
+    const existedUser= await User.findOne({
         $or:[{username}, {email}]
     })
     if(existedUser){
@@ -63,7 +63,14 @@ const registerUser=asyncHandler(async(req,res)=>{
 
     //4
     const avatarLocalPath= req.files?.avatar[0]?.path;
-    const coverImageLocalPath= req.files?.coverImage[0]?.path;
+    // const coverImageLocalPath= req.files?.coverImage[0]?.path;
+
+    let coverImageLocalPath;
+
+    if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length>0){
+        coverImageLocalPath=req.files.coverImage[0].path
+    }
+
 
     if(!avatarLocalPath){
         throw new ApiError(400,"Avatar is required")}
